66a5d372f455022b5330bf3bc542a28e76c9a34c
Alfa2k24 <alfa.sunny.2k24@gmail.com>
2024-05-11 13:13:07 +0600

    Solution Accepted

--- A___Tiger__vs__Pathan.c
+++ A___Tiger__vs__Pathan.c
@@ -1,5 +1,34 @@
 #include<stdio.h>
 int main(){
+    int T;
+    scanf("%d",&T);
+    for(int t = 1; t <= T; t++){
 
+    int n;
+    scanf("%d",&n);
+    char str[n+1];
+    scanf("%s",str);
+    int T = 0, P = 0;
+    for(int i = 0;i < n; i++){
+        if(str[i] == 'T'){
+            T++;
+        } else{
+            P++;
+        }
+    }
+
+    if(T < P){
+        printf("Pathaan");
+    } else if( T > P){
+        printf("Tiger");
+    } else{
+        printf("Draw");
+    }
+
+    printf("\n");
+
+    }
     return 0;
 }
+
+// Solution Accepted
