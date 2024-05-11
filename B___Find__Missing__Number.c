1732041894fb92074d795f046595af27fcec778f
Alfa2k24 <alfa.sunny.2k24@gmail.com>
2024-05-11 13:48:45 +0600

    Solution Accepted

--- B___Find__Missing__Number.c
+++ B___Find__Missing__Number.c
@@ -1,5 +1,45 @@
 #include<stdio.h>
 int main(){
+int T;
+scanf("%d",&T);
+for(int t = 0; t < T; t++){
 
-    return 0;
+long long int M,Value,A,B,C,D;
+
+
+scanf("%lld %lld %lld %lld", &M, &A, &B, &C);
+ Value = A*B*C;
+
+ if(Value == 0 || M == 0){
+
+    printf("0");
+
+ } 
+ 
+ else{
+
+    if(M%Value == 0){
+        D = M / Value;
+        printf("%lld",D);
+    } else if(M%Value != 0){
+        printf("-1");
+    }
+ }
+
+printf("\n");
+
 }
+return 0;
+}
+
+// Solution Accepted 
+
+//----------------------------------------------
+
+    // D = M / Value;
+    // printf("D = %d\n",D);
+    // if(Value * D == M){
+    // printf("%d", D);
+    // } else{
+    // printf("-1");
+    // }
